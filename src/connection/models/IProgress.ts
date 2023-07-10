9b27cefb7430435890806bcf018cb9317652b42e
Matheus Portela <108295503+mpm-10@users.noreply.github.com>
2023-07-10 15:38:03 -0300

    Implementação da interface IProgress
    Arquivo de implementação da interface "IProgress", necessária para a obtenção dos dados do MongoDB através do Mongoose.

---
+++ src/connection/models/IProgress.ts
@@ -0,0 +1,19 @@
+
+
+export default interface IProgress
+{
+    nickname : String,
+    password : String,
+    time_game : Number,
+    lifes : Number,
+    score : Number,
+    level : Number,
+    experience : Number,
+    coins : Number,
+    power1 : Number,
+    power2 : Number,
+    life : Number,
+    stamina : Number, 
+    shield : Number,
+    armor : Number
+}
